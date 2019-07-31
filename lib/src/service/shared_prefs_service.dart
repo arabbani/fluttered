f2dd9b54ae6d7b72d2a3904b016f4092e9e5a984
Arif Rabbani <arif.93rabbani@gmail.com>
2019-07-31 14:31:29 +0530

    SharedPrefsService added

--- lib/src/service/shared_prefs_service.dart
+++ lib/src/service/shared_prefs_service.dart
@@ -1,5 +1,8 @@
+import 'package:fluttered/src/util/misc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Manage the persistent storage. Used to store simple data
+/// as key-value pair.
 class SharedPrefsService {
   static SharedPrefsService _instance;
   static SharedPreferences _preferences;
@@ -12,5 +15,32 @@ class SharedPrefsService {
       _preferences = await SharedPreferences.getInstance();
     }
     return _instance;
+  }
+
+  /// Saves the [value] to persistent storage.
+  ///
+  /// If [value] is null, this wil remove the [key] entry from persistent storage.
+  ///
+  /// Supported [value] types: `int`, `double`, `String`, `bool` and `List<String>`,
+  void set<T>(String key, T value) {
+    if (value is int) {
+      _preferences.setInt(key, value);
+    } else if (value is double) {
+      _preferences.setDouble(key, value);
+    } else if (value is String) {
+      _preferences.setString(key, value);
+    } else if (value is bool) {
+      _preferences.setBool(key, value);
+    } else if (value is List<String>) {
+      _preferences.setStringList(key, value);
+    } else {
+      assert(false,
+          '(ERROR) Fluttered_SharedPrefsService:: set() => unsupported value type');
+    }
+    printTrace('set() => key: $key value: $value');
+  }
+
+  T get<T>(String key) {
+    return _preferences.get(key);
   }
 }
---
+++ lib/src/util/misc.dart
@@ -0,0 +1,7 @@
+printTrace(String data) {
+  print('(TRACE) Fluttered_SharedPrefsService:: $data');
+}
+
+printError(String data) {
+  print('(ERROR) Fluttered_SharedPrefsService:: $data');
+}
